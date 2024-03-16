3696302f520fcac74e4433efff753722a5a32a46
kle <01fe22bca105@kletech.ac.in>
2024-03-17 02:09:26 +0530

    Main code

---
+++ studentcourse.java
@@ -0,0 +1,27 @@
+
+
+public class studentcourse{
+    
+    String[] Course = {"Machine_learning","Software_testing","C_Sharp","Devops"};
+    float[] marks = {88,35,38,66};
+
+  
+    void get_course(){
+    System.out.println("Courses Registered:");
+        for(int i=0;i<Course.length;i++){
+            
+            System.out.println("\t"+Course[i]);
+            
+        }
+
+    }
+void get_marks(){
+
+        for(int i=0;i<Course.length;i++){
+            if(marks[i]<40){
+
+            System.out.println(Course[i] +"="+marks[i]);
+        }
+    }
+}
+}
